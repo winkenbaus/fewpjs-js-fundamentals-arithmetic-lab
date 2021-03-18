cc4cc78c33200ce4149cc6fd95bfd4788b67beb8
caitlin <caitlin.wink@gmail.com>
2021-03-18 11:39:47 -0700

    commit tree a995a8631560670cda1aa82c8573d601c3a23c29

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1*num2);
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+let num3 = 12
+let num4 = 8
+let mod = (num3%num4);
+
+let max = Math.max(5,10,15,20);
+
+
